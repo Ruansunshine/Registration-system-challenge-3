e79fb72b337d612798d5ee2f3431066e3a9fecff
ruan <ruandasilvasantos49@gmail.com>
2025-04-07 19:35:11 -0300

    Desafio 3: Adicionando validações na pagina password, funções no arquivo formatters

--- modules/formatters.js
+++ modules/formatters.js
@@ -93,24 +93,48 @@ function formatadorNumero() {
 
 function formatadorSenha() {
     let senha = document.getElementById("senha");
-    senha.addEventListener('input', function (e) {
+    let senhaConfirmada = document.getElementById("senha-confirmada");
 
-        const temMaiuscula = /[A-Z]/.test(valor);
-        const temNumero = /[0-9]/.test(valor);
-        const temEspecial = /[!@#$%^&*(),.?":{}|<>]/.test(valor);
 
-        const maiuscula = document.getElementById("maiuscula");
-        const caractere = document.getElementById("caractere-especial");
-        const numero = document.getElementById("numero");
+    let maiuscula = document.getElementById("maiuscula");
+    let caractere = document.getElementById("caractere-especial");
+    let numero = document.getElementById("numero");
+    let senhaIgual = document.getElementById("senha-igual");
+
+    function validarSenha(valor) {
+
+        let temMaiuscula = /[A-Z]/.test(valor);
+        let temNumero = /[0-9]/.test(valor);
+        let temEspecial = /[!@#$%^&*(),.?":{}|<>]/.test(valor);
 
         maiuscula.style.color = temMaiuscula ? "green" : "red";
         caractere.style.color = temEspecial ? "green" : "red";
         numero.style.color = temNumero ? "green" : "red";
+    }
 
+    
+    function senhasIguais() {
+        if (senha.value === senhaConfirmada.value && senha.value.length > 0) {
+            senhaIgual.style.color = "green";
+            const dados = JSON.parse(localStorage.getItem('dadosInscricao')) || {};
 
+            localStorage.setItem('dadosInscricao', JSON.stringify(dados));
+            dados.senha = senha.value;
 
+            alert("Redirecionando...");
+            window.location.href = '../index.html';
+        } else {
+            senhaIgual.style.color = "red";
+        }
+    }
+   senha.addEventListener('input', ()=> {
+    validarSenha(senha.value);
+    senhasIguais();
+   });
+   senhaConfirmada.addEventListener('input', ()=> {
+    senhasIguais();
+   });
 
-    })
 }
 
 export { formatadorCpf, formatadorEmail, formatadorTelefone, formatadorCep, formatadorNumero, formatadorSenha };
--- modules/mainPassword.js
+++ modules/mainPassword.js
@@ -1,5 +1,11 @@
-import { formatadorSenha } from "./Formatters";
+
+//main password
+import { formatadorSenha } from "./formatters.js";
+import { fazerLogin } from "./functions.js";
 
 
-
+//formatters.js
 formatadorSenha();
+
+//functions.js
+fazerLogin();
